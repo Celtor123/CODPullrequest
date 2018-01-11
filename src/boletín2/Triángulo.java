518d512440ecdce7c719dc5124f57798abea08d3
oracle <oracle@victoria25.danielcastelao.org>
2018-01-11 09:32:58 +0100

    EEEEEEEEEEEEEEEEEEEEE

---
+++ src/boletín2/Triángulo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletín2;
+
+/**
+ *
+ * @author oracle
+ */
+public class Triángulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int base = 4;
+        int altura= 3;
+        System.out.println((base*altura)/2);
+        
+    }
+    
+}
